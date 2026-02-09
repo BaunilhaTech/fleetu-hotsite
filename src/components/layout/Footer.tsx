fc8d9ce015e3d2fcb993357971cff61c62c61cc9
Alexandre Alvaro <alexandre.alvaro@hotmail.com>
2026-02-09 13:22:48 -0300

    feat: remove redundant Fleetu link paragraph from footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,18 +12,6 @@ export function Footer() {
             <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row mx-auto px-4 md:px-8">
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
                     <Image src={logo} alt="Fleetu Logo" width={22} height={24} className="w-auto h-6" />
-                    <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                        {" "}
-                        <a
-                            href="#"
-                            target="_blank"
-                            rel="noreferrer"
-                            className="font-medium underline underline-offset-4"
-                        >
-                            Fleetu
-                        </a>
-                        .
-                    </p>
                 </div>
                 <p className="text-center text-sm text-muted-foreground">
                     &copy; {new Date().getFullYear()} Fleetu. {t("copyright")}
